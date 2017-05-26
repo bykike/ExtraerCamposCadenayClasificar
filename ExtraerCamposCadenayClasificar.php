eaba888db33f6fcb847c70ec18b5da31f3025686
Kike <kike@areavisual.net>
2017-05-26 13:04:21 +0200

    Cambios

--- ExtraerCamposCadenayClasificar.php
+++ ExtraerCamposCadenayClasificar.php
@@ -1,63 +1,21 @@
 <?php
 
-    /* $Especialidad=trim($registro['Especialidad']); */
+    $Especialidad = "1,2,3,4,5,6,7,8,";
+    $Especialidad = trim($Especialidad);
+    $Especialidades=explode(",",$Especialidad);
 
-    $Especialidad = trim("1,2,3,4,5,6");
-    $Especialidades=explode(",",$Especialidad); 
 
+    for($i=0;$i<count($Especialidades);$i++){
 
-    echo $Especialidad . "<br>";
+        if ($Especialidades[$i] == "1") { echo "Preparador físico especialista en rendimiento deportivo" . "<br>"; }
+        if ($Especialidades[$i] == "2") { echo "Preparador físico como educador físico o readaptador de lesiones, patologías o enfermedades". "<br>"; }
+        if ($Especialidades[$i] == "3") { echo "Especialista en pruebas de acceso a cuerpos y fuerzas de seguridad del estado" . "<br>"; }
+        if ($Especialidades[$i] == "4") { echo "Profesor de educación física" . "<br>"; }
+        if ($Especialidades[$i] == "5") { echo "Director deportivo especialista en gestión deportiva" . "<br>"; }
+        if ($Especialidades[$i] == "6") { echo "Especialista en actividades en el medio natural" . "<br>"; }
+        if ($Especialidades[$i] == "7") { echo "Especialista en fitness y actividades dirigidas en salas polivalentes" . "<br>"; }
+        if ($Especialidades[$i] == "8") { echo "Especialista en natación, actividades acuáticas o salvamento y socorrismo" . "<br>"; }
 
-    echo $Especialidades . "<br>";
-
-    echo $Especialidades[0];
-
-    if (!empty($Especialidades[0])) 
-        {
-            for($i=0;$i<count($Especialidades);$i++){
-
-                switch ($Especialidades[$i]) {
-
-                    case '1':
-                        echo "Preparador físico especialista en rendimiento deportivo";
-                        echo "<br";
-                        break;
-                    case '2':
-                        echo "Preparador físico como educador físico o readaptador de lesiones, patologías o enfermedades";
-                        echo "<br"; 
-                        break;  
-                    case "3":
-                        echo "Especialista en pruebas de acceso a cuerpos y fuerzas de seguridad del estado";
-                        echo "<br";
-                        break;
-                    case "4":
-                        echo "Profesor de educación física";
-                        echo "<br"; 
-                        break; 
-                    case "5":
-                        echo "Director deportivo especialista en gestión deportiva";
-                        echo "<br";
-                        break;
-                    case "6":
-                        echo "Especialista en actividades en el medio natural";
-                        echo "<br";
-                        break;  
-                    case "7":
-                        echo "Especialista en fitness y actividades dirigidas en salas polivalentes";
-                        echo "<br";
-                        break;
-                    case "8":
-                        echo "Especialista en natación, actividades acuáticas o salvamento y socorrismo";
-                        echo "<br"; 
-                         break;                                                             
-                    default:
-                         # code...
-                         break;
-
-                 } /* Fin Switch */
-
-                } /* Fin for */
+    } 
         
-        } /* Fin If */ 
- 
 ?>
